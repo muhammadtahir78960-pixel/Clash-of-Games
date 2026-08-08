03f2b6f58d846e9fa5a017f1a97d1f3579df6433
muhammadtahir78960-pixel <muhammadtahir78960@gmail.com>
2026-08-08 20:11:06 +0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,20 @@
+appname="Clash of Games"
+appver="1.0"
+appcode="1"
+appsdk="15"
+path_pattern=""
+packagename="com.tahirht.clashofgames"
+theme="Theme"
+app_key=""
+app_channel=""
+developer="Tahir HT"
+description=""
+debugmode=true
+user_permission={
+  "ACCESS_NETWORK_STATE",
+  "ACCESS_WIFI_STATE",
+  "INTERNET",
+  "READ_EXTERNAL_STORAGE",
+  "VIBRATE",
+  "WRITE_EXTERNAL_STORAGE"
+}
